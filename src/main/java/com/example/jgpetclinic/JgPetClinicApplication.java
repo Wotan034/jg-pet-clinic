3eee70f9d9f4581c3e33919b115a1d9e3c497538
Wotan034 <john.gibney034@gmail.com>
2020-11-07 11:45:57 -0500

    Initial commit

---
+++ src/main/java/com/example/jgpetclinic/JgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.jgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JgPetClinicApplication.class, args);
+    }
+
+}
